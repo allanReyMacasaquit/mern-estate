79afdbdb1883fd10fd0e416c0283820777c4ce56
allan <allanrey.macasaquit@gmail.com>
2023-10-10 18:05:09 -0400

    2. Create Routes and Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/about';
+import Profile from './pages/profile';
+import Signup from './pages/signup';
+import Signin from './pages/Signin';
+
 export default function App() {
-	return <div className='text-red-500'>Real Estate Management Sysytem</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/profile' element={<Profile />} />
+				<Route path='/sign-up' element={<Signup />} />
+				<Route path='/sign-in' element={<Signin />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
